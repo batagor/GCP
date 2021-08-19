752ecab481eaf666c9baf8a261d44b11c3b76e23
Muhajirin <muhajirin@homecredit.co.id>
2021-08-19 02:47:30 -0400

    remove output public ip

--- modules/compute-engine-noip/outputs.tf
+++ modules/compute-engine-noip/outputs.tf
@@ -2,8 +2,3 @@ output "private_ip" {
   description = "list private ip on compute instance"
   value       = ["${google_compute_instance.recruitement.*.network_interface.0.network_ip}"]
 }
-
-output "public_ip" {
-  description = "list private ip on compute instance"
-  value       = ["${google_compute_instance.recruitement.*.network_interface.0.access_config.0.nat_ip}"]
-}
