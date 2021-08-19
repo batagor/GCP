53dcea0eba1fef7f526a3ba7c2c303335ea95d0b
Muhajirin <muhajirin@homecredit.co.id>
2021-08-19 00:40:58 -0400

    provision app vm revision 3

--- main.tf
+++ main.tf
@@ -2,14 +2,14 @@ module "vm_instance_1" {
   source  = "airasia/vm_instance/google"
   version = "2.13.0"
   boot_disk_image = "global/images/family/centos-8"
-  instance_name = "vm-recrut-app-01"
+  instance_name = "vmapp01"
   name_suffix = "recruitement"
   network_tags = ["recruit-app"]
   vpc_subnetwork = "alpha1-vpc"
   allow_login = true
   boot_disk_size = 20
   create_external_ip = true
-  external_ip_name = "vm-recrut-app-01-pip"
+  external_ip_name = "vmrecrutapp01pip"
   fw_allowed_ports = ["22"]
   machine_type = "e2-micro"
 }
