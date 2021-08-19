42dfe78bc43f2572250842939f0cf598482d15c9
Muhajirin <muhajirin@homecredit.co.id>
2021-08-19 00:37:53 -0400

    provision app vm revision 2

--- main.tf
+++ main.tf
@@ -4,12 +4,12 @@ module "vm_instance_1" {
   boot_disk_image = "global/images/family/centos-8"
   instance_name = "vm-recrut-app-01"
   name_suffix = "recruitement"
-  network_tags = "recruit-app"
+  network_tags = ["recruit-app"]
   vpc_subnetwork = "alpha1-vpc"
   allow_login = true
   boot_disk_size = 20
   create_external_ip = true
   external_ip_name = "vm-recrut-app-01-pip"
-  fw_allowed_ports = 22
+  fw_allowed_ports = ["22"]
   machine_type = "e2-micro"
 }
